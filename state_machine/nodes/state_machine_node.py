e358ce3ddcedecc9bb993afa29c0bc6083fa7e67
Nick Trivelis <ntrivelis@gmail.com>
2020-05-31 21:27:05 -0400

    Added state machine for STEVE

--- state_machine/nodes/state_machine_node.py
+++ state_machine/nodes/state_machine_node.py
@@ -3,10 +3,9 @@
 import rospy
 import smach
 
-def main():
-    rospy.init_node('state_machine')
 
-    # Create the state machine
+def steve_state_machine():
+    # Create an empty state machine
     sm = smach.StateMachine(outcomes=['END'])
 
     # Add states to the state machine
@@ -14,6 +13,41 @@ def main():
         smach.StateMachine.add(
             'START',
             StartState(),
+            transitions={'outcome1':'POWERUP'}
+        )
+        smach.StateMachine.add(
+            'POWERUP',
+            PowerUpState(),
+            transitions={'outcome1':'RELEASE'}
+        )
+        smach.StateMachine.add(
+            'RELEASE',
+            ReleaseState(),
+            transitions={'outcome1':'SPLASHDOWN'}
+        )
+        smach.StateMachine.add(
+            'SPLASHDOWN',
+            SplashdownState(),
+            transitions='outcome1':'IDLE'}
+        )
+        smach.StateMachine.add(
+            'IDLE',
+            IdleState(),
+            transitions='outcome1':'COMMS', 'outcome2':'EXPLORE', 'outcome3':'POWERDOWN'}
+        )
+        smach.StateMachine.add(
+            'COMMS',
+            CommsStateMachine(),
+            transitions={'outcome1':'IDLE'}
+        )
+        smach.StateMachine.add(
+            'EXPLORE',
+            ExploreStateMachine(),
+            transitions={'outcome1':'IDLE'}
+        )
+        smach.StateMachine.add(
+            'POWERDOWN',
+            PowerDownStateMachine(),
             transitions={'outcome1':'END'}
         )
         smach.StateMachine.add(
@@ -21,6 +55,14 @@ def main():
             EndState(),
             transitions={'outcome1':'END'}
         )
+    return sm
+
+
+def main():
+    rospy.init_node('state_machine')
+
+    # Create the state machine
+    sm = steve_state_machine()
 
     # Execute the state machine plan
     outcome = sm.execute()
