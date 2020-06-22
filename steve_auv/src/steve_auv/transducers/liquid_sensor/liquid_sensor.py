6e6f955dd8224c681c6d4649b3af4c57a2bbbec1
dcoll5420 <daniel.r.collins@colorado.edu>
2020-06-21 19:24:37 -0600

    Serial communication from rpi to arduino

---
+++ steve_auv/src/steve_auv/transducers/liquid_sensor/liquid_sensor.py
@@ -0,0 +1,6 @@
+import serial
+arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=.1)
+while True:
+    data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
+    if data:
+        print(data)
