6418af7b1ee1c2946ee175e53f87dbfdf5d6c2ce
Nick Trivelis <ntrivelis@gmail.com>
2020-08-23 18:27:16 -0400

    Added test images for ellipse detection

--- steve_auv/src/steve_auv/vision/utils/ellipse_detector.py
+++ steve_auv/src/steve_auv/vision/utils/ellipse_detector.py
@@ -40,35 +40,43 @@ def main():
                               "steve_auv/data/coins.jpg"),
                              cv2.IMREAD_COLOR
                             )
-
     print(image.shape)
-    detector = EllipseDetector()
 
-    image = cv2.blur(image, (10, 10)) 
-    image = cv2.dilate(image, np.ones((10, 10), np.uint8), iterations=1)
-    image = cv2.blur(image, (10, 10))
+    from skimage import color, img_as_ubyte
+    from skimage.feature import canny
+    from skimage_transform import hough_ellipse
+    from skimage.draw import ellipse_perimeter
+
+    edges = canny(image, sigma=2.0, low_threshold=0.55, high_threshold=0.8)
+    result = hough_ellipse(edges, accuracy=20, threshold=250, mmin_size=100, max_size=120)
+    result.sort(order='accumulator')
+
+    #detector = EllipseDetector()
+
+    #image = cv2.blur(image, (10, 10)) 
+    #image = cv2.dilate(image, np.ones((10, 10), np.uint8), iterations=1)
+    #image = cv2.blur(image, (10, 10))
 
     #_, image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY_INV)
-    image = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 2)
-    _, contours, hierarchy = cv2.findContours(image, 2, 1)
-    keep = []
-    for i in contours:
-        area = cv2.contourArea(i)
-        if area > 50000:
-            keep.append(i)
-    print(len(keep))
-    """
-    keypoints = detector.detect(image)
-    image_out = cv2.drawKeypoints(
-                    image,
-                    keypoints,
-                    np.zeros((1, 1)),
-                    (0, 0, 255),
-                    cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS
-                )
-    """
-    image_final = cv2.drawContours(image_final, keep, -1, (0, 255, 0), 3)
-    cv2.imshow("Ellipses", image_final)
+    #image = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+    #_, contours, hierarchy = cv2.findContours(image, 2, 1)
+    #keep = []
+    #for i in contours:
+    #    area = cv2.contourArea(i)
+    #    if area > 50000:
+    #        keep.append(i)
+    #print(len(keep))
+    #keypoints = detector.detect(image)
+    #image_out = cv2.drawKeypoints(
+    #                image,
+    #                keypoints,
+    #                np.zeros((1, 1)),
+    #                (0, 0, 255),
+    #                cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS
+    #            )
+    #
+    #image_final = cv2.drawContours(image_final, keep, -1, (0, 255, 0), 3)
+    cv2.imshow("Ellipses", edges)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
