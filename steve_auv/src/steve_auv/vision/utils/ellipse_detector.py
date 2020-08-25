52d6b24373427f5d18e65e7512d90602cd25646f
Nick Trivelis <ntrivelis@gmail.com>
2020-08-24 23:45:42 -0400

    Test images finished

--- steve_auv/src/steve_auv/vision/utils/ellipse_detector.py
+++ steve_auv/src/steve_auv/vision/utils/ellipse_detector.py
@@ -4,8 +4,14 @@
 ####################################
 
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
-#import rospy
+import os
+import rospy
+
+from skimage.feature import canny
+from skimage.io import imread
+from skimage.transform import resize
 
 
 class EllipseDetector(object):
@@ -27,143 +33,8 @@ class EllipseDetector(object):
 
     def detect(self, image):
         #rospy.loginfo("Running ellipse detection.")
-        keypoints = self._detector.detect(image)
-        return keypoints
-
-
-def main():
-    #image = cv2.imread(("/home/pi/workspaces/steve/src/steve/steve_auv/test/"
-    #                    "steve_auv/data/coins.jpg"),
-    #                   cv2.IMREAD_GRAYSCALE
-    #                  )
-    #image_final = cv2.imread(("/home/pi/workspaces/steve/src/steve/steve_auv/test/"
-    #                          "steve_auv/data/coins.jpg"),
-    #                         cv2.IMREAD_COLOR
-    #                        )
-    import cv2
-    import skimage
-    from skimage import color, img_as_ubyte
-    from skimage.feature import canny
-    from skimage.transform import hough_ellipse, resize
-    from skimage.draw import ellipse_perimeter
-    import matplotlib.pyplot as plt
-    import numpy as np
-
-    image = skimage.io.imread(("/home/cloud-user/repos/scuba_steve/steve_auv/test/"
-                               "steve_auv/data/coins.jpg"),
-                               as_gray=True
-                             )
-    image_final = skimage.io.imread(("/home/cloud-user/repos/scuba_steve/steve_auv/test/"
-                                     "steve_auv/data/coins.jpg"),
-                                     as_gray=False
-                                   )
-    image = resize(image, (image.shape[0] // 8, image.shape[1] // 8), anti_aliasing=True)
-    print(image.shape)
-    image_final = resize(image_final, (image_final.shape[0] // 8, image_final.shape[1] // 8), anti_aliasing=True)
-    #plt.imshow(image, cmap=plt.cm.gray)
-    #plt.show()
-
-    edges = canny(image, sigma=1.00, low_threshold=0.60, high_threshold=0.90)
-    #plt.imshow(edges)
-    #plt.show()
-
-    #result = hough_ellipse(edges, threshold=250, accuracy=20, min_size=25)
-    #result.sort(order='accumulator')
-
-    # Estimated parameters for the ellipse
-    #best = list(result[-1])
-    #yc, xc, xa, xb = [int(round(x)) for x in best[1:5]]
-    #orientation = best[5]
-
-    # Draw the ellipse on the original image
-    #cy, cx = ellipse_perimeter(yc, xc, xa, xb, orientation)
-    #import pdb; pdb.set_trace()
-    #image_final[cy, cx] = (0, 0, 255)
-
-    # Draw the edge (white) and the resulting ellipse (red)
-    #edges = color.gray2rgb(img_as_ubyte(edges))
-    #edges[cy, cx] = (250, 0, 0)
-
-    #def auto_canny(image, sigma=0.1):
-    #    # compute the median of the single channel pixel intensities
-    #    v = np.median(image)
-    #    # apply automatic Canny edge detection using the computed median
-    #    lower = int(max(0, (1.0 - sigma) * v))
-    #    upper = int(min(255, (1.0 + sigma) * v))
-    #    edged = cv2.Canny(image, lower, upper)
-    #    # return the edged image
-    #    return edged
-
-    #image = cv2.imread(("/home/cloud-user/repos/scuba_steve/steve_auv/test/"
-    #                    "steve_auv/data/coins.jpg")
-    #                  )
-    #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    #gray = cv2.resize(gray, (gray.shape[1] // 8, gray.shape[0] // 8))
-    #edges = auto_canny(gray)
-    #plt.imshow(edges, cmap=plt.cm.gray)
-    #plt.show()
-
-    detector = EllipseDetector()
-    edges = np.asarray(255 * edges, np.uint8)
-    _, edges = cv2.threshold(edges, 127, 255, cv2.THRESH_BINARY)
-
-    def findEllipses(edges):
-        contours, _ = cv2.findContours(edges.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-        ellipseMask = np.zeros(edges.shape, dtype=np.uint8)
-        contourMask = np.zeros(edges.shape, dtype=np.uint8)
-
-        pi_4 = np.pi * 4
-
-        for i, contour in enumerate(contours):
-            if len(contour) < 5:
-                continue
-
-            area = cv2.contourArea(contour)
-            if area <= 100:  # skip ellipses smaller then 10x10
-                continue
-
-            arclen = cv2.arcLength(contour, True)
-            circularity = (pi_4 * area) / (arclen * arclen)
-            ellipse = cv2.fitEllipse(contour)
-            poly = cv2.ellipse2Poly((int(ellipse[0][0]), int(ellipse[0][1])), (int(ellipse[1][0] / 2), int(ellipse[1][1] / 2)), int(ellipse[2]), 0, 360, 5)
-
-            # if contour is circular enough
-            if circularity > 0.6:
-                cv2.fillPoly(ellipseMask, [poly], 255)
-                continue
-
-            # if contour has enough similarity to an ellipse
-            similarity = cv2.matchShapes(poly.reshape((poly.shape[0], 1, poly.shape[1])), contour, cv2.cv.CV_CONTOURS_MATCH_I2, 0)
-            if similarity <= 0.2:
-                cv2.fillPoly(contourMask, [poly], 255)
-
-        return ellipseMask, contourMask    
-
-    def _fit_ellipse(thresholded_image):
-        """Finds contours and fits an ellipse to thresholded image
-    
-        Parameters
-        ----------
-        thresholded_image :
-            Binary image containing two eyes
-
-        Returns
-        -------
-        type
-            When eyes were found, the two ellipses, otherwise False
-
-        """
-        cont_ret = cv2.findContours(
-            thresholded_image.astype(np.uint8), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
-        )
-
-        # API change, in OpenCV 4 there are 2 values unlike OpenCV3
-        if len(cont_ret) == 3:
-            _, contours, hierarchy = cont_ret
-        else:
-            contours, hierarchy = cont_ret
-    
-        if len(contours) >= 2:
+        _, contours, _ = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        if len(contours) >= 1:
             n = len(contours)
             # Get the two largest ellipses (i.e. the eyes, not any dirt)
             contours = sorted(contours, key=lambda c: c.shape[0], reverse=True)[:n]
@@ -171,76 +42,80 @@ def main():
             contours = sorted(contours, key=np.max)
     
             # Fit the ellipses for the two eyes
-            if len(contours[0]) > 4 and len(contours[1]) > 4:
-                e = [cv2.fitEllipse(contours[i]) for i in range(n)]
-                return e
-            else:
-                return False
-    
+            e = []
+            for i in contours:
+                if len(i) > 4:
+                    e.append(cv2.fitEllipse(i))
+            return e
         else:
             # Not at least two eyes + maybe dirt found...
             return False
-    
-    ellipses = _fit_ellipse(edges)
-    import pdb; pdb.set_trace()
-    #contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    #keep = []
-    #for i in contours:
-    #    area = cv2.contourArea(i)
-    #    if area > 100:
-    #        keep.append(i)
-    #keypoints = detector.detect(edges)
-    #image_out = cv2.drawKeypoints(
-    #                edges,
-    #                keypoints,
-    #                np.zeros((1, 1)),
-    #                (0, 0, 255),
-    #                cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS
-    #            )
-    import copy 
-    image_final_out = copy.deepcopy(image_final) 
-    #image_final_out = cv2.drawContours(image_final_out, keep, -1, (0, 0, 255), 3)
-    print(len(ellipses))
-    for i in ellipses:
-        cv2.ellipse(image_final_out, i, (0, 0, 255))
-    fig2, (ax1, ax2) = plt.subplots(ncols=2, nrows=1, figsize=(8, 4),
-                                    sharex=True, sharey=True)
-    ax1.set_title('Original picture')
-    ax1.imshow(image_final)
-    ax2.set_title('Edge (white) and result (red)')
-    ax2.imshow(image_final_out)
-    plt.show()
 
-    #detector = EllipseDetector()
 
-    #image = cv2.blur(image, (10, 10)) 
-    #image = cv2.dilate(image, np.ones((10, 10), np.uint8), iterations=1)
-    #image = cv2.blur(image, (10, 10))
+def main():
+    imgs = [
+            '/home/pi/workspaces/steve/src/steve/steve_auv/test/steve_auv/data/coins.jpg',
+            '/home/pi/workspaces/steve/src/steve/steve_auv/test/steve_auv/data/IMG_2951.jpg',
+            '/home/pi/workspaces/steve/src/steve/steve_auv/test/steve_auv/data/IMG_2952.jpg',
+            '/home/pi/workspaces/steve/src/steve/steve_auv/test/steve_auv/data/IMG_2953.jpg',
+            '/home/pi/workspaces/steve/src/steve/steve_auv/test/steve_auv/data/IMG_2954.jpg',
+            '/home/pi/workspaces/steve/src/steve/steve_auv/test/steve_auv/data/IMG_2955.jpg',
+            '/home/pi/workspaces/steve/src/steve/steve_auv/test/steve_auv/data/IMG_2956.jpg',
+            '/home/pi/workspaces/steve/src/steve/steve_auv/test/steve_auv/data/IMG_2958.jpg',
+            '/home/pi/workspaces/steve/src/steve/steve_auv/test/steve_auv/data/IMG_2959.jpg',
+            '/home/pi/workspaces/steve/src/steve/steve_auv/test/steve_auv/data/IMG_2960.jpg',
+            '/home/pi/workspaces/steve/src/steve/steve_auv/test/steve_auv/data/IMG_2961.jpg'
+           ]
+    #imgs = ['/home/pi/workspaces/steve/src/steve/steve_auv/test/steve_auv/data/IMG_2952.jpg']
+    out_dir = '/home/pi'
+    for img in imgs:
+        image = imread(img, as_gray=True)
+        image_final = imread(img, as_gray=False)
+        sf = 8.0
+        scale = (image.shape[0] / (image.shape[0] // sf), image.shape[1] / (image.shape[1] // sf))
+        shape_origin = image.shape
+        image = resize(image, (image.shape[0] // sf, image.shape[1] // sf), anti_aliasing=True)
 
-    #_, image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY_INV)
-    #image = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 2)
-    #_, contours, hierarchy = cv2.findContours(image, 2, 1)
-    #keep = []
-    #for i in contours:
-    #    area = cv2.contourArea(i)
-    #    if area > 50000:
-    #        keep.append(i)
-    #print(len(keep))
-    #keypoints = detector.detect(image)
-    #image_out = cv2.drawKeypoints(
-    #                image,
-    #                keypoints,
-    #                np.zeros((1, 1)),
-    #                (0, 0, 255),
-    #                cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS
-    #            )
-    #
-    #image_final = cv2.drawContours(image_final, keep, -1, (0, 255, 0), 3)
-    #cv2.imshow("Ellipses", edges)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
-    #plt.imshow(edges, cmap=plt.cm.gray)
-    #plt.show()
+        #from skimage.filters import threshold_otsu
+        #thresh = threshold_otsu(image)
+        #plt.imshow(image, cmap=plt.cm.gray)
+        #plt.show()
+        edges = canny(image, sigma=.5, low_threshold=0.60, high_threshold=0.80)
+        #from scipy import ndimage as ndi
+        #edges = ndi.binary_fill_holes(edges)
+        #from skimage.morphology import dilation
+        #edges = dilation(edges)
+        #from skimage.filters import sobel
+        #edges = sobel(image)
+        #markers = np.zeros_like(image)
+        #markers[image < 30] = 1
+        #markers[image > 150] = 2
+        #from skimage.segmentation import watershed
+        #edges = watershed(edges, markers)
+        #plt.imshow(edges, cmap=plt.cm.gray)
+        #plt.show()
+        edges = np.asarray(255 * edges, np.uint8)
+        #print(np.min(edges))
+        #print(np.max(edges))
+        #edges = np.asarray(255 * edges, np.uint8)
+        _, edges = cv2.threshold(edges, 127, 255, cv2.THRESH_BINARY)
+        edges_final = resize(edges, shape_origin, anti_aliasing=True)
+        #plt.imshow(edges, cmap=plt.cm.gray)
+        #plt.show()
+
+        detector = EllipseDetector()
+        ellipses = detector.detect(edges)
+        if ellipses != False:
+            for i in ellipses:
+                i = ((i[0][0] * scale[0], i[0][1] * scale[1]), (i[1][0] * scale[0], i[1][1] * scale[1]), i[2])
+                cv2.ellipse(image_final, i, (255, 0, 0), 4)
+            fig, (ax1, ax2) = plt.subplots(ncols=2, nrows=1, figsize=(8, 4), sharex=True, sharey=True)
+            ax1.set_title('Canny Edge Detection')
+            ax1.imshow(edges_final)
+            ax2.set_title('Fitted Ellipses')
+            ax2.imshow(image_final)
+            filename = os.path.splitext(os.path.split(img)[1])[0]
+            plt.savefig(os.path.join(out_dir, filename + "_ellipses.jpg"))
 
 
 if __name__ == "__main__":
